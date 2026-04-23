cc915210295fb7877d2f70dff2a5001b7ffd143f
CEOofYap <johncenavstherock0126@gmail.com>
2026-04-24 02:43:35 +0800

    feat: search function with brave

---
+++ util⚡🛠.ts
@@ -0,0 +1,72 @@
+// test-ilmu.js
+// const response = await fetch("https://api.ilmu.ai/v1/chat/completions", {
+//     method: "POST",
+//     headers: {
+//         Authorization: `Bearer sk-ee4d162134a084aba30b9410e8055081c6a064a2cb7d0d22`,
+//         "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//         model: "ilmu-glm-5.1",
+//         messages: [{ role: "user", content: "Hello from Bun!" }],
+//     }),
+// });
+
+// const data = await response.json();
+// console.log(data.choices?.[0]?.message?.content || data);
+
+import OpenAI from "openai";
+
+// const client = new OpenAI({
+//     apiKey: "sk-ee4d162134a084aba30b9410e8055081c6a064a2cb7d0d22",
+//     baseURL: "https://api.ilmu.ai/v1",
+// });
+
+// const response = await client.chat.completions.create({
+//     model: "ilmu-glm-5.1",
+//     messages: [{ role: "user", content: "Explain quicksort in 3 sentences." }],
+// });
+
+// console.log(response.choices[0].message.content);
+
+/**
+ * Accepts query search it using Brave API
+ * @param query Search query
+ * @param num number of result returned
+ * @param date Search result until today
+ * @returns JSON object of the results
+ */
+async function search(query: string, num: number = 20, date: Date) {
+    const today: string = new Date().toISOString().split("T")[0];
+    const freshness: string = date.toISOString().split("T")[0] + "to" + today;
+    try {
+        const res = await fetch(
+            "https://api.search.brave.com/res/v1/llm/context",
+            {
+                method: "POST",
+                headers: {
+                    "X-Subscription-Token": process.env.BRAVE_API,
+                    accept: "application/json",
+                    "Accept-Encoding": `gzip`,
+                    "Content-Type": `application/json`,
+                },
+                body: JSON.stringify({
+                    q: query,
+                    count: num,
+                    freshness: freshness,
+                }),
+            },
+        );
+        const results: JSON = await res.json();
+        return results;
+    } catch (err) {
+        console.log("API problem");
+        console.log(err);
+    }
+}
+
+const results = await search(
+    "Black detective conan",
+    20,
+    new Date("2026-1-20"),
+);
+console.log(JSON.stringify(results, null, 4));
